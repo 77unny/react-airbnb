30a25497a2903788e6bbfbb298d1e84b60803104
baekCode <sosaer88@gmail.com>
2020-05-29 02:42:58 +0900

    feat: skeleton container 구현
    issue #5

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyle';
 import Theme from './styles/Theme';
+import SearchFormContainer from './containers/SearchFormContainer';
+import SearchListContainer from './containers/SearchListContainer';
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
+      <SearchFormContainer />
+      <SearchListContainer />
     </ThemeProvider>
   );
 };
---
+++ FE/src/containers/SearchListContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchListContainer = () => {
+  return <div></div>;
+};
+
+export default SearchListContainer;
