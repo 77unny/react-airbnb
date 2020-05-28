1d09e9278ed6c5d4c8864379c6da005efe6e289b
baekCode <sosaer88@gmail.com>
2020-05-29 04:32:59 +0900

    feat: 날짜 선택시 날짜 검색 버튼 날짜 출력
    issue #9

--- FE/src/components/SearchForm/SearchButtons.jsx
+++ FE/src/components/SearchForm/SearchButtons.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { SEARCH_FORM } from '../../constants';
 
@@ -11,9 +12,16 @@ const SearchButton = ({ formType, onOpenForm, title }) => {
 };
 
 const SearchButtons = ({ onOpenForm }) => {
+  const { checkInDate, checkOutDate } = useSelector(state => state.date);
+  const dateCompleted = `${checkInDate} - ${checkOutDate}`;
+
   return (
     <SearchButtonDiv>
-      <SearchButton formType={SEARCH_FORM.TYPE.DATE} title="날짜" onOpenForm={onOpenForm} />
+      <SearchButton
+        formType={SEARCH_FORM.TYPE.DATE}
+        title={checkInDate ? (checkOutDate ? dateCompleted : checkInDate) : '날짜'}
+        onOpenForm={onOpenForm}
+      />
       <SearchButton formType={SEARCH_FORM.TYPE.GUEST} title="인원" onOpenForm={onOpenForm} />
       <SearchButton formType={SEARCH_FORM.TYPE.PRICE} title="요금" onOpenForm={onOpenForm} />
       <button>검색</button>
