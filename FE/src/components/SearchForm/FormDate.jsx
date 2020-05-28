5b45398ee4ba4f94261adf164c18d4e219a99548
baekCode <sosaer88@gmail.com>
2020-05-29 04:31:48 +0900

    feat: 달력 컴포넌트 구현 (react-dates 라이브러리 적용)
    issue #9

--- FE/src/components/SearchForm/FormDate.jsx
+++ FE/src/components/SearchForm/FormDate.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkDate, reset } from '../../modules/date';
+import styled from 'styled-components';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+const ResetBtn = styled.button`
+  position: relative;
+  z-index: 1;
+`;
 
 const FormDate = () => {
-  return <div>날짜</div>;
+  const dispatch = useDispatch();
+  const { startDate, endDate } = useSelector(state => state.date);
+  const [focus, setFocus] = useState('startDate');
+  const handleOnDateChange = date => dispatch(checkDate(date));
+  const resetButton = () => <ResetBtn onClick={() => dispatch(reset())}>초기화</ResetBtn>;
+  return (
+    <div>
+      <DateRangePicker
+        startDatePlaceholderText=""
+        startDate={startDate}
+        onDatesChange={handleOnDateChange}
+        endDatePlaceholderText=""
+        endDate={endDate}
+        numberOfMonths={2}
+        showClearDates={true}
+        focusedInput={focus}
+        onFocusChange={focus => setFocus(focus)}
+        startDateId="startDateMookh"
+        endDateId="endDateMookh"
+        minimumNights={0}
+        renderCalendarInfo={resetButton}
+        hideKeyboardShortcutsPanel
+        readOnly
+        keepOpenOnDateSelect
+      />
+    </div>
+  );
 };
 
 export default FormDate;
