1cdcc0fc3f1c7161612aceb2238e27250b12c2f0
baekCode <sosaer88@gmail.com>
2020-05-29 13:51:54 +0900

    feat: 인원 상수값
    issue #10

--- FE/src/constants/index.js
+++ FE/src/constants/index.js
@@ -5,3 +5,20 @@ export const SEARCH_FORM = {
     PRICE: 'price',
   },
 };
+export const GUEST = {
+  ADULTS: {
+    TYPE: 'adults',
+    TITLE: '성인',
+    SUBTITLE: '만 13세 이상',
+  },
+  CHILDREN: {
+    TYPE: 'children',
+    TITLE: '성인',
+    SUBTITLE: '2 ~ 12세',
+  },
+  INFANTS: {
+    TYPE: 'infants',
+    TITLE: '유아',
+    SUBTITLE: '2세 미만',
+  },
+};
