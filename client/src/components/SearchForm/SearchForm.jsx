94fcc9bacc9782bee6fbfb7390c5e51250dc9dae
baekCode <sosaer88@gmail.com>
2020-06-06 09:54:43 +0900

    style: SearchForm 스타일링

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -4,8 +4,10 @@ import { SEARCH_FORM } from '../../constants';
 import FormDate from './FormDate';
 import FormGuest from './FormGuest';
 import FormPrice from './FormPrice';
+import SearchButtons from './SearchButtons';
 
 const SearchFormDiv = styled.div`
+  position: relative;
   overflow: hidden;
   width: 100%;
   height: 0;
@@ -19,17 +21,47 @@ const SearchFormDiv = styled.div`
 
 const SearchFormContentsDiv = styled.div`
   padding: 20px 0;
+  text-align: center;
 `;
 
 const SearchFormCloseBtn = styled.button`
   position: absolute;
   top: 20px;
   right: 10%;
+  width: 32px;
+  height: 32px;
+  text-indent: -999em;
+  border: 0;
+  outline: none;
+  cursor: pointer;
+  background: none;
+  opacity: 0.3;
+  transition: all 0.3s;
+  &:before,
+  &:after {
+    position: absolute;
+    top: 0;
+    left: 15px;
+    content: ' ';
+    height: 33px;
+    width: 2px;
+    background-color: #333;
+  }
+  &:before {
+    transform: rotate(45deg);
+  }
+  &:after {
+    transform: rotate(-45deg);
+  }
+  &:hover {
+    opacity: 1;
+  }
 `;
 
-const SearchForm = ({ isOpen, viewContents, onCloseForm }) => {
+const SearchForm = ({ isOpen, viewContents, onOpenForm, onCloseForm }) => {
   return (
     <SearchFormDiv open={isOpen}>
+      <SearchButtons viewContents={viewContents} onOpenForm={onOpenForm} />
       {viewContents === SEARCH_FORM.TYPE.DATE && (
         <SearchFormContentsDiv>
           <FormDate />
